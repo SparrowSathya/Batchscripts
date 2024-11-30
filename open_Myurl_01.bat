0c9088cb2142f9b68be2e58c8ea11168f654c25e
SparrowSathya <sparrowsathiya@gmail.com>
2024-11-30 11:51:51 +0530

    cmd : Customize Which Browser to View Url Multiple Browsers

--- open_Myurl_01.bat
+++ open_Myurl_01.bat
@@ -2,5 +2,5 @@
 title Open_Url_01 Batch Script by Sathya.
 echo This My Bash Script To Open MyUrl.
 pause
-start www.google.com
-start www.youtube.com/@sparrowsathya8278
+start firefox.exe www.google.com
+start msedge.exe www.youtube.com/@sparrowsathya8278
