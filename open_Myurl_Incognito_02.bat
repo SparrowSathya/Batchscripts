ed7e06e0b53cda3539241477a0764258ae98ea95
SparrowSathya <66060892+SparrowSathya@users.noreply.github.com>
2024-12-01 09:40:05 +0530

    cmd : Added Google Chrome URL

--- open_Myurl_Incognito_02.bat
+++ open_Myurl_Incognito_02.bat
@@ -4,3 +4,4 @@ title Open_URL_INCOGNITO_02
 pause
 start /d "C:\Program Files\Mozilla Firefox\" firefox.exe -private-window www.google.com
 start msedge.exe -inprivate www.youtube.com/@sparrowsathya8278
+start chrome.exe -incognito www.facebook.com
