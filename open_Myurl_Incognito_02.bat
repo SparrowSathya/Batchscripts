017057a6e0d461e1f6c8ab6bc126aa2abfb5f418
SparrowSathya <sparrowsathiya@gmail.com>
2024-12-01 09:36:19 +0530

    cmd : Added Code For URL's Open in Incognito or private windows script

---
+++ open_Myurl_Incognito_02.bat
@@ -0,0 +1,6 @@
+@echo off
+echo This My Bash Script To Open MyUrl Incognito in Different Browsers and Different Mode.
+title Open_URL_INCOGNITO_02
+pause
+start /d "C:\Program Files\Mozilla Firefox\" firefox.exe -private-window www.google.com
+start msedge.exe -inprivate www.youtube.com/@sparrowsathya8278
