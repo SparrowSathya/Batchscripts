cbc6fab558197f11443a036d866ffb7efb2b0c7e
SparrowSathya <sparrowsathiya@gmail.com>
2024-12-03 08:08:17 +0530

    Feat : Modified Cmd Promt Color Change Script options Reset to default & exit options.

--- MyOWn_Code/Cmd_Color.bat
+++ MyOWn_Code/Cmd_Color.bat
@@ -1,16 +1,51 @@
 @echo off
-title Cmd_Color
+title Enhanced Cmd_Color
 cls
-echo This Scrit to Change the Command Prompt Color.
-help color
-:start
-set /p user_input="Choose Color (A to F) & (1 to 9): "
+:main_menu
+echo ===========================
+echo      CMD COLOR CHANGER     
+echo ===========================
+echo Available Colors:
+echo 0 = Black       8 = Gray
+echo 1 = Blue        9 = Light Blue
+echo 2 = Green       A = Light Green
+echo 3 = Aqua        B = Light Aqua
+echo 4 = Red         C = Light Red
+echo 5 = Purple      D = Light Purple
+echo 6 = Yellow      E = Light Yellow
+echo 7 = White       F = Bright White
+echo ===========================
+echo Format: BackgroundColor + TextColor (e.g., 0A, 1F, etc.)
+echo ===========================
+echo [R] Reset to Default
+echo [E] Exit
+echo ===========================
+set /p user_input="Enter your choice: "
 
-echo "You can Combine Selection "81" 8=Background Color & 1=Text Color"
+REM Validate Input
+if "%user_input%"=="" goto main_menu
+if /i "%user_input%"=="R" goto reset
+if /i "%user_input%"=="E" goto exit
 
-echo You are Choosing %user_input% color wil be Changed.
-pause
+ 
+
+REM Apply Color
+echo Changing color to %user_input%...
 COLOR %user_input%
-echo Color Changed Successfully !
+echo Color changed successfully!
 pause
-goto :start
+cls
+goto main_menu
+
+:reset
+echo Resetting to default color...
+COLOR 07
+echo Default color restored!
+pause
+cls
+goto main_menu
+
+:exit
+echo Exiting program. Goodbye!
+pause
+exit
